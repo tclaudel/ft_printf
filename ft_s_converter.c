e7cde13cff699550fe3e2daddbcab3db73e98d00
Thomas Claudel <tclaudel@z2r1p3.le-101.fr>
2019-12-02 12:29:57 +0100

    normed

--- ft_s_converter.c
+++ ft_s_converter.c
@@ -6,7 +6,7 @@
 /*   By: tclaudel <tclaudel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/28 10:38:30 by tclaudel     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/02 11:50:13 by tclaudel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/02 12:29:42 by tclaudel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,7 +19,6 @@ char	*ft_s_converter(char *str, size_t accu, char *flags)
 		return (ft_strdup(""));
 	if (!str)
 		str = "(null)";
-		
 	if (ft_char_in_string('.', flags) && accu < ft_strlen(str))
 		return (ft_strndup(str, accu));
 	return (ft_strdup(str));
