fcb45890a9a8e557c9d0c5214b1c01292a52a000
Thomas Claudel <tclaudel@z2r1p3.le-101.fr>
2019-12-02 12:28:27 +0100

    .s, 42 patched

--- ft_s_converter.c
+++ ft_s_converter.c
@@ -6,7 +6,7 @@
 /*   By: tclaudel <tclaudel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/28 10:38:30 by tclaudel     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/18 11:15:24 by tclaudel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/02 11:50:13 by tclaudel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,8 +15,11 @@
 
 char	*ft_s_converter(char *str, size_t accu, char *flags)
 {
+	if (ft_char_in_string('.', flags) && accu == 0)
+		return (ft_strdup(""));
 	if (!str)
 		str = "(null)";
+		
 	if (ft_char_in_string('.', flags) && accu < ft_strlen(str))
 		return (ft_strndup(str, accu));
 	return (ft_strdup(str));
